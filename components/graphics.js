202df57bfce13c8382cd268518cb7a8c15585853
tzkallo <kallotamas@gmail.com>
2025-09-02 20:45:29 +0200

    -

---
+++ components/graphics.js
@@ -0,0 +1,14 @@
+const Graphics = {
+    template: '#Graphics',
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+            technologies: [
+                { name: 'AutoCAD', color: 'blue lighten-3' },
+                { name: '3dsMax', color: 'deep-purple accent-3' },
+                { name: 'Drawing', color: 'cyan darken-2' },
+                { name: 'Photoshop', color: 'blue darken-3' },
+            ]
+        }
+    }
+};
